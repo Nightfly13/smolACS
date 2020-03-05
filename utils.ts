2903eb17b5009b604694a83f623c6a538b6f0dd7
Sebski@Laptop <sebthomle@gmail.com>
2020-03-05 12:19:50 +0100

    Hack for sending connectionInfo to db

--- utils.ts
+++ utils.ts
@@ -1,5 +1,6 @@
 import { writeFileSync } from "fs";
 import { CpeGetResponse } from "./interfaces";
+import * as http from "http"
 
 export function writeResponseToFile(cpeResponse: CpeGetResponse): void {
   let fileName = cpeResponse.name + ".json";
@@ -26,6 +27,42 @@ export function writeResponseToFile(cpeResponse: CpeGetResponse): void {
   writeFileSync(fileName, data, { flag: "a" })
 }
 
-export function getConnectionInfo(GetParameterValuesResponse: CpeGetResponse){
-  throw Error("NotImplemented")
+export function getConnectionInfo(GetParameterValuesResponse: CpeGetResponse) {
+  let parameterList = GetParameterValuesResponse.parameterList as [string, string | number | boolean, string][]
+  let freq5: boolean = true
+  let freq2: boolean = true
+
+  if (!parameterList[0][0].includes("Device.WiFi.AccessPoint.1.")) { freq2 = false }
+  if (!parameterList[0][0].includes("Device.WiFi.AccessPoint.5.")) { freq5 = false }
+  if (!freq2 && !freq5) { return }
+
+  let numEntries: number = parameterList.find((parameter) => parameter[0] == `Device.WiFi.AccessPoint.${freq5 ? "5" : "1"}.AssociatedDeviceNumberOfEntries`)[1] as number
+
+  if (numEntries == 0) { return }
+
+  let devices: { deviceMac: string; freq: number; signal_str: number; }[] = []
+
+  for (let index = 1; index <= numEntries; index++) {
+    let macAddr: string = parameterList.find((parameter) => parameter[0] == `Device.WiFi.AccessPoint.${freq5 ? "5" : "1"}.AssociatedDevice.${index}.MACAddress`)[1] as string
+    let signalStr: number = parameterList.find((parameter) => parameter[0] == `Device.WiFi.AccessPoint.${freq5 ? "5" : "1"}.AssociatedDevice.${index}.SignalStrength`)[1] as number
+    devices.push({ deviceMac: macAddr, freq: freq5 ? 50 : 24, signal_str: signalStr })
+  }
+
+
+  let reqOpts = {
+    hostname: '127.0.0.1',
+    port: 7555,
+    path: '/addConnection',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }
+
+  devices.forEach(async device => {
+    let data = JSON.stringify(device)
+    let req = http.request(reqOpts)
+    req.write(data)
+    req.end()
+  });
 }
